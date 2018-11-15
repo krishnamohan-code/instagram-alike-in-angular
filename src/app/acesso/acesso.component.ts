07c8044d37718698ecbd229368dc4b3ba1e548eb
Carlos Wosiak <carloswosiak@outlook.com>
2018-11-15 14:17:43 -0200

    add animation to the images in the login page

--- src/app/acesso/acesso.component.ts
+++ src/app/acesso/acesso.component.ts
@@ -1,11 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-acesso',
   templateUrl: './acesso.component.html',
-  styleUrls: ['./acesso.component.scss']
+  styleUrls: ['./acesso.component.scss'],
+  animations: [
+    trigger('animacao-banner', [
+      state('criado', style({
+        opacity: 1
+      })),
+      transition('void => criado', [
+        style({ opacity: 0, transform: 'translate(0, -100px)'}),
+        animate( '800ms 0s ease-in-out')
+      ])
+    ])
+  ]
 })
 export class AcessoComponent implements OnInit {
+  public estadoBanner = 'criado';
 
   constructor() { }
 
